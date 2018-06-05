1715cce8cc5ca4d173ab8607a154beca823c1d94
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-06-04 23:42:20 -0700

    finishing app

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,6 +10,21 @@ var friends = [
     name: "Dwight",
     photo: "https://pbs.twimg.com/profile_images/1863312401/Dwight.jpg",
     answers: [3,5,3,2,2,3,1,4,5,5]
+  },
+  {
+    name: "Edward",
+    photo: "https://i.pinimg.com/originals/d1/1e/8a/d11e8ad5b7cd44b8449c2c283c0ec59a.jpg",
+    answers: [2,1,1,2,2,3,1,4,2,5]
+  },
+  {
+    name: "LeBron James",
+    photo: "https://i1.wp.com/www.nationalreview.com/wp-content/uploads/2018/05/RTX66A5E.jpg",
+    answers: [4,1,3,1,2,1,1,4,5,5]
+  },
+  {
+    name: "Farley",
+    photo: "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a_400x400.jpeg",
+    answers: [2,5,2,5,5,1,4,5,5,1]
   }
 ];
 
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,59 +1,50 @@
 
 var friends = require("../app/data/friends");
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-// var friends = require("../data/friends");
-// var friends = require("../data/waitinglistData");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(friends);
-  });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the friends array)
-  // ---------------------------------------------------------------------------
+  app.post("/api/friends", function(req, res) {
 
-  app.post("/api/newfriend", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    
+      var newFriend = req.body;
+      var newFriendScore = newFriend.answers.map(Number);
+      var bestMatchIndex = 0
+      var lowestScore = sumValuesArrays(newFriendScore, friends[0].answers);
+
+
+      for (let index = 1; index < friends.length; index++) {
+        
+        if(lowestScore > sumValuesArrays(newFriendScore, friends[index].answers)) {
+
+          lowestScore = sumValuesArrays(newFriendScore, friends[index].answers);
+          bestMatchIndex = index;
+
+          
+        }
+
+      }
+
       friends.push(req.body);
-      res.json(true);
+
+
+      res.json(friends[bestMatchIndex]);
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friends = [];
-
-    console.log(friends);
-  });
+  
 };
+
+function sumValuesArrays(arr1, arr2) {
+  var total = 0;
+  for (let index = 0; index < arr1.length; index++) {
+
+    total += Math.abs(parseInt(arr1[index]) - parseInt(arr2[index]));
+    
+  }
+
+  return total;
+
+}
